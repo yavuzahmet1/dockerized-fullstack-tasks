0c5cd172ebb9d84e5ee62dbe3613e4fa70891351
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-02-23 23:23:58 +0300

    feat: add Task entity with fields and methods for task management

---
+++ backend/src/main/java/com/devtry/tasks/entities/Task.java
@@ -0,0 +1,138 @@
+package com.devtry.tasks.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name="due_date")
+    private LocalDateTime dueDate;
+
+    @Column(name="status")
+    private TaskStatus status;
+
+    @Column(name="priority")
+    private TaskPriority priority;
+
+    @Column(name="created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name="updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    public Task(UUID id,String title, String description, LocalDateTime dueDate, TaskPriority priority,  TaskStatus status, LocalDateTime createdAt, LocalDateTime updatedAt   ) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+@Override
+public int hashCode() {
+    return (id == null) ? 0 : id.hashCode();
+}
+
+@Override
+public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null || getClass() != obj.getClass()) return false;
+
+    Task other = (Task) obj;
+    return id != null && id.equals(other.id);
+}
+
+@Override
+public String toString() {
+    return "Task [id=" + id + ", title=" + title + ", description=" + description + ", dueDate=" + dueDate + ", status="
+            + status + ", priority=" + priority + ", createdAt=" + createdAt + ", updatedAt=" + updatedAt + "]";
+}
+
+}
