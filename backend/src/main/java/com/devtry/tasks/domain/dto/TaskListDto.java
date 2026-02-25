4dad709ed8340bbcb6f8dfe6b6218508b953000f
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-02-25 22:10:28 +0300

    feat: create TaskListDto record for task list data transfer

---
+++ backend/src/main/java/com/devtry/tasks/domain/dto/TaskListDto.java
@@ -0,0 +1,13 @@
+package com.devtry.tasks.domain.dto;
+
+import java.util.List;
+import java.util.UUID;
+
+public record TaskListDto(
+                UUID id,
+                String title,
+                String description,
+                Integer count,
+                Double progress,
+                List<TaskDto> tasks) {
+}
