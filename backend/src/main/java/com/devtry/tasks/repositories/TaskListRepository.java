95f750c106be793baa8f7b05c5a51bb81d1f1d21
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-02-27 18:22:45 +0300

    feat: create TaskListRepository interface for TaskList entity management

---
+++ backend/src/main/java/com/devtry/tasks/repositories/TaskListRepository.java
@@ -0,0 +1,12 @@
+package com.devtry.tasks.repositories;
+
+import com.devtry.tasks.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+
+}
