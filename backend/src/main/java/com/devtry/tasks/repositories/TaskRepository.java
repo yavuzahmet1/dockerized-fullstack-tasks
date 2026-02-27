7ecddf4ff5de76f5363f231c86bd7cb305a8a1e2
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-02-27 18:22:52 +0300

    feat: add TaskRepository interface for managing Task entities

---
+++ backend/src/main/java/com/devtry/tasks/repositories/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.devtry.tasks.repositories;
+
+import com.devtry.tasks.domain.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByTaskListId(UUID taskListId);
+    Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
+}
