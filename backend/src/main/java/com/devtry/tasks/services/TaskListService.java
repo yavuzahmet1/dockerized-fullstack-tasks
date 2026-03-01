89aab9d1ea42d6e8bd676aa3e7a6316dd5fcd545
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-01 15:16:37 +0300

    feat: update TaskListService interface to include createTaskList method

--- backend/src/main/java/com/devtry/tasks/services/TaskListService.java
+++ backend/src/main/java/com/devtry/tasks/services/TaskListService.java
@@ -1,11 +1,11 @@
 package com.devtry.tasks.services;
 
 import com.devtry.tasks.domain.entities.TaskList;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface TaskListService {
-List<TaskList> taskListLists();
+    List<TaskList> taskListLists();
+    TaskList createTaskList(TaskList taskList);
+
 }
