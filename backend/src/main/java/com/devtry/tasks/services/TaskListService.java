0d7b02f920592efb654712fa3dd7497bd63b8499
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-03 22:09:11 +0300

    feat: add getTaskListById method to TaskListService interface for retrieving a task list by ID

--- backend/src/main/java/com/devtry/tasks/services/TaskListService.java
+++ backend/src/main/java/com/devtry/tasks/services/TaskListService.java
@@ -3,9 +3,12 @@ package com.devtry.tasks.services;
 import com.devtry.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface TaskListService {
     List<TaskList> taskListLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskListById(UUID taskListId);
 
 }
