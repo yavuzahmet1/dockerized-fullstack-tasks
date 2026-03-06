f1b9887de10c4737b7c862f977d4b5a90b42605d
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-06 22:57:43 +0300

    refactor: remove unused TaskMapper dependency and clean up imports in TaskController

--- backend/src/main/java/com/devtry/tasks/controller/TaskController.java
+++ backend/src/main/java/com/devtry/tasks/controller/TaskController.java
@@ -1,14 +1,13 @@
 package com.devtry.tasks.controller;
 
 import com.devtry.tasks.domain.dto.TaskDto;
+import com.devtry.tasks.domain.entities.Task;
 import com.devtry.tasks.mappers.TaskMapper;
 import com.devtry.tasks.services.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -16,14 +15,22 @@ import java.util.UUID;
 public class TaskController {
 
     private final TaskService taskService;
-    private final TaskMapper taskMapper;
 
-    public TaskController(TaskService taskService, TaskMapper taskMapper) {
+    public TaskController(TaskService taskService) {
         this.taskService = taskService;
-        this.taskMapper = taskMapper;
+
     }
     @GetMapping
     public List<TaskDto> listTasks(@PathVariable UUID taskListId) {
         return taskService.listTasks(taskListId);
+    }
+
+    @PostMapping
+    public TaskDto createTask(@PathVariable UUID taskListId, @RequestBody TaskDto taskDto) {
+        return taskService.createTask(taskListId, taskDto);
+    }
+    @GetMapping(path = "/{taskId}")
+    public Optional<TaskDto> getTaskById(@PathVariable UUID taskListId, @PathVariable UUID taskId) {
+        return taskService.getTaskById(taskListId, taskId);
     }
 }
