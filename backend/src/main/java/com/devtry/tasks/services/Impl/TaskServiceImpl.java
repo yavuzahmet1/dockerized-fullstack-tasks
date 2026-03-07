5c7648c2bf3f9e4db2a703ac095aa7c059670840
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-07 18:54:09 +0300

    refactor: reorganize imports and enhance method implementations in TaskServiceImpl

--- backend/src/main/java/com/devtry/tasks/services/Impl/TaskServiceImpl.java
+++ backend/src/main/java/com/devtry/tasks/services/Impl/TaskServiceImpl.java
@@ -1,5 +1,10 @@
 package com.devtry.tasks.services.Impl;
 
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.stereotype.Service;
 import com.devtry.tasks.domain.dto.TaskDto;
 import com.devtry.tasks.domain.entities.Task;
 import com.devtry.tasks.domain.entities.TaskList;
@@ -9,62 +14,70 @@ import com.devtry.tasks.mappers.TaskMapper;
 import com.devtry.tasks.repositories.TaskListRepository;
 import com.devtry.tasks.repositories.TaskRepository;
 import com.devtry.tasks.services.TaskService;
-import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
 
 @Service
 public class TaskServiceImpl implements TaskService {
-    private final TaskRepository taskRepository;
-    private final TaskMapper taskMapper;
-    private final TaskListRepository taskListRepository;
+  private final TaskRepository taskRepository;
+  private final TaskMapper taskMapper;
+  private final TaskListRepository taskListRepository;
 
-    public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper, TaskListRepository taskListRepository) {
-        this.taskRepository = taskRepository;
-        this.taskMapper = taskMapper;
-        this.taskListRepository = taskListRepository;
+  public TaskServiceImpl(TaskRepository taskRepository, TaskMapper taskMapper,
+      TaskListRepository taskListRepository) {
+    this.taskRepository = taskRepository;
+    this.taskMapper = taskMapper;
+    this.taskListRepository = taskListRepository;
+  }
+
+  @Override
+  public List<TaskDto> listTasks(UUID taskListId) {
+    return taskRepository.findByTaskListId(taskListId).stream().map(taskMapper::toDto).toList();
+  }
+
+  @Override
+  public TaskDto createTask(UUID taskListId, TaskDto taskDto) {
+    if (null != taskDto.id()) {
+      throw new IllegalArgumentException("Task already exists!");
+    }
+    if (taskDto.title() == null || taskDto.title().isBlank()) {
+      throw new IllegalArgumentException("Task title required!");
+    }
+    TaskPriority taskPriority = Optional.ofNullable(taskDto.priority()).orElse(TaskPriority.MEDIUM);
+
+    TaskStatus taskStatus = TaskStatus.OPEN;
+
+    TaskList taskList = taskListRepository.findById(taskListId)
+        .orElseThrow(() -> new IllegalArgumentException("Invalid task listId!"));
+
+    Task taskToSave = new Task(null, taskDto.title(), taskDto.description(), taskDto.dueDate(),
+        taskStatus, taskPriority, taskList);
+    taskRepository.save(taskToSave);
+    return taskMapper.toDto(taskToSave);
+  }
+
+  @Override
+  public Optional<TaskDto> getTaskById(UUID taskListId, UUID taskId) {
+    return taskRepository.findByTaskListIdAndId(taskListId, taskId).map(taskMapper::toDto);
+  }
+
+  @Override
+  public TaskDto updateTask(UUID taskListId, UUID taskId, TaskDto taskDto) {
+    if (null == taskDto.id()) {
+      throw new IllegalArgumentException("Task must have an ID!");
+    }
+    if (!Objects.equals(taskId, taskDto.id())) {
+      throw new IllegalArgumentException("Task IDs do not match");
     }
 
-    @Override
-    public List<TaskDto> listTasks(UUID taskListId) {
-        return taskRepository.findByTaskListId(taskListId)
-                .stream()
-                .map(taskMapper::toDto)
-                .toList();
-    }
+    Task existingTask = taskListRepository.findByTaskListIdAndId(taskListId, taskId)
+        .orElseThrow(() -> new IllegalArgumentException("Task not found!"));
 
-    @Override
-    public TaskDto createTask(UUID taskListId, TaskDto taskDto) {
-        if (null != taskDto.id()) {
-            throw new IllegalArgumentException("Task already exists!");
-        }
-        if (taskDto.title() == null || taskDto.title().isBlank()) {
-            throw new IllegalArgumentException("Task title required!");
-        }
-        TaskPriority taskPriority=Optional.ofNullable(taskDto.priority())
-                .orElse(TaskPriority.MEDIUM);
+    existingTask.setTitle(taskDto.title());
+    existingTask.setDescription(taskDto.description());
+    existingTask.setDueDate(taskDto.dueDate());
+    existingTask.setPriority(taskDto.priority());
+    existingTask.setStatus(taskDto.status());
 
-        TaskStatus taskStatus=TaskStatus.OPEN;
-
-        TaskList taskList=taskListRepository.findById(taskListId).orElseThrow(()->new IllegalArgumentException("Invalid task listId!"));
-
-        Task taskToSave=new Task(
-                null,taskDto.title(),
-                taskDto.description(),
-                taskDto.dueDate(),
-                taskStatus,taskPriority,taskList
-        );
-        taskRepository.save(taskToSave);
-        return taskMapper.toDto(taskToSave);
-    }
-
-    @Override
-    public Optional<TaskDto> getTaskById(UUID taskListId, UUID taskId) {
-        return taskRepository
-                .findByTaskListIdAndId(taskListId, taskId)
-                .map(taskMapper::toDto);
-    }
+    Task savedTask = taskRepository.save(existingTask);
+    return taskMapper.toDto(savedTask);
+  }
 }
