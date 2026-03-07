e21676955992f1343f04da1522055b7c74b59324
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-07 18:53:57 +0300

    refactor: reorganize imports and add findByTaskListIdAndId method in TaskListRepository

--- backend/src/main/java/com/devtry/tasks/repositories/TaskListRepository.java
+++ backend/src/main/java/com/devtry/tasks/repositories/TaskListRepository.java
@@ -1,12 +1,13 @@
 package com.devtry.tasks.repositories;
 
-import com.devtry.tasks.domain.entities.TaskList;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.UUID;
+import com.devtry.tasks.domain.entities.Task;
+import com.devtry.tasks.domain.entities.TaskList;
 
 @Repository
 public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
-
+  Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
 }
