8b4738363efbc1e5da028fc45f4fee928bfbd8d4
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-07 18:54:03 +0300

    refactor: reorder methods in TaskService interface for improved readability

--- backend/src/main/java/com/devtry/tasks/services/TaskService.java
+++ backend/src/main/java/com/devtry/tasks/services/TaskService.java
@@ -1,14 +1,16 @@
 package com.devtry.tasks.services;
 
-import com.devtry.tasks.domain.dto.TaskDto;
-import com.devtry.tasks.domain.entities.Task;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import com.devtry.tasks.domain.dto.TaskDto;
 
 public interface TaskService {
-    List<TaskDto> listTasks(UUID taskListId);
-    TaskDto createTask(UUID taskListId, TaskDto taskDto);
-    Optional<TaskDto> getTaskById(UUID taskListId, UUID taskId);
+  List<TaskDto> listTasks(UUID taskListId);
+
+  TaskDto createTask(UUID taskListId, TaskDto taskDto);
+
+  Optional<TaskDto> getTaskById(UUID taskListId, UUID taskId);
+
+  TaskDto updateTask(UUID taskListId, UUID taskId, TaskDto taskDto);
 }
