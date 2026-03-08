f5cff430bddd8048015cd5dbe09a47b0f444011a
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-08 12:24:17 +0300

    refactor: remove unused import and method from TaskListRepository

--- backend/src/main/java/com/devtry/tasks/repositories/TaskListRepository.java
+++ backend/src/main/java/com/devtry/tasks/repositories/TaskListRepository.java
@@ -1,13 +1,11 @@
 package com.devtry.tasks.repositories;
 
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.devtry.tasks.domain.entities.Task;
 import com.devtry.tasks.domain.entities.TaskList;
 
 @Repository
 public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
-  Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
+
 }
