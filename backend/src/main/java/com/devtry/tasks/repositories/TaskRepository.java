33fee5349ee451ff2ea6f5d4d6720f4b5b59a26c
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-08 12:24:22 +0300

    refactor: reorganize imports and improve formatting in TaskRepository

--- backend/src/main/java/com/devtry/tasks/repositories/TaskRepository.java
+++ backend/src/main/java/com/devtry/tasks/repositories/TaskRepository.java
@@ -1,15 +1,20 @@
 package com.devtry.tasks.repositories;
 
-import com.devtry.tasks.domain.entities.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.devtry.tasks.domain.entities.Task;
 
 @Repository
 public interface TaskRepository extends JpaRepository<Task, UUID> {
-    List<Task> findByTaskListId(UUID taskListId);
-    Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
+  List<Task> findByTaskListId(UUID taskListId);
+
+  Optional<Task> findByTaskListIdAndId(UUID taskListId, UUID id);
+
+  void deleteByTaskListIdAndId(UUID taskListId, UUID id);
+
+
+
 }
