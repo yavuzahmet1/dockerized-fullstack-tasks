b8b849a86a36b4c67e4c48c69f2b4b26fa7fe69e
yavuzahmet1 <1yavuzahmet@gmail.com>
2026-03-08 12:24:29 +0300

    refactor: adjust formatting and reorder imports in TaskListService

--- backend/src/main/java/com/devtry/tasks/services/TaskListService.java
+++ backend/src/main/java/com/devtry/tasks/services/TaskListService.java
@@ -1,16 +1,19 @@
 package com.devtry.tasks.services;
 
-import com.devtry.tasks.domain.entities.TaskList;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import com.devtry.tasks.domain.entities.TaskList;
 
 public interface TaskListService {
-    List<TaskList> taskListLists();
-    TaskList createTaskList(TaskList taskList);
-    Optional<TaskList> getTaskListById(UUID taskListId);
-    TaskList updateTaskList(UUID taskListId, TaskList taskList);
-    void deleteTaskListById(UUID taskListId);
+  List<TaskList> taskListLists();
+
+  TaskList createTaskList(TaskList taskList);
+
+  Optional<TaskList> getTaskListById(UUID taskListId);
+
+  TaskList updateTaskList(UUID taskListId, TaskList taskList);
+
+  void deleteTaskListById(UUID taskListId);
 
 }
